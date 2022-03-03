3eabe22c4af62358cf4d84ec82d3ed0bb30667cc
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-03-03 17:25:08 +0100

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get "/users", to: "users#index" 
-  get "/users/:id", to: "users#show"
-  get "/posts", to: "posts#index"
-  get "/posts/:id", to: "posts#show"
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
