58a2a44ac0c80f89adb080f32fd18ef38825996b
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-03-03 17:53:29 +0100

    Fix linters issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
-
+  def show; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
   end
-
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
