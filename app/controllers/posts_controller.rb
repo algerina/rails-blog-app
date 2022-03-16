97445499c6cde40f68bbfe5dd5178034ebebf695
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-03-16 16:01:23 +0100

    Create route controller and view for form and new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,24 @@ class PostsController < ApplicationController
     @post_id = params[:id]
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @current_user = User.find(params[:user_id])
+    post = Post.new(post_params.merge(author_id: @current_user.id, comments_counter: 0, likes_counter: 0))
+    if post.valid?
+      post.save
+      redirect_to user_path(@current_user.id)
+    else
+      redirect_to new_user_post_path(@current_user.id)
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
