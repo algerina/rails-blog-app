21df01e34e011ccf393fe545126aeb3458f53930
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-03-17 22:03:01 +0100

    test users module

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.new( name: ' ', photo: 'https://images.pexels.com/photos/6171563/pexels-photo-6171563.jpeg', bio: 'Artist from wonderland', posts_counter: 1 )
+  end
+
+  before { subject.save }
+
+  it 'should have a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Posts counter should not be nil' do
+    subject.posts_counter = nil
+    expect(subject).to_not be_valid
+  end
+
 end
+
+
