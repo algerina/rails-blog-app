aecc3300e1077fefbaaee1fd4aeb299bcbbeb27f
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-03-18 19:19:46 +0100

    Update comments flash

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,13 +10,12 @@ class CommentsController < ApplicationController
 
     if @comment.valid?
       @comment.save
-      # redirect_to user_post_path(id: @post.id, user_id: @user.id)
       flash.notice = 'Comment added successfuly.'
+      redirect_to user_post_path(@user.id, @post.id)
     else
-      # redirect_to new_user_post_comment_path(@user.id, @post.id)
       flash.alert = 'Please try again.'
+      redirect_to new_user_post_comment_path(@user.id, @post.id)
     end
-    redirect_to new_user_post_comment_path(@user.id, @post.id)
   end
 
   def post_params
