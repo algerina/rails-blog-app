8824215d102ceb54aade3fd6bd4a58173084a291
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-03-22 23:26:08 +0100

    Add sign_out button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
-    # @user = current_user
+    # @user = User.find(params[:id])
+    @user = current_user
     @posts = @user.most_recent_posts
   end
 end
