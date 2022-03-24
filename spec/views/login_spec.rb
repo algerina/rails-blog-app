4ad258957ecef5dadb5922abf6234ee6a1ec1c4b
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-03-24 19:24:20 +0100

    Create user_index_spec file

--- spec/views/login_spec.rb
+++ spec/views/login_spec.rb
@@ -1,22 +1,5 @@
 require 'rails_helper'
 
-# describe "the signin process", type: :feature do
-#     before :each do
-#       User.create(email: 'user@example.com', password: 'password')
-#     end
-
-#     it "signs me in" do
-#       visit '/users/sign_in'
-#       within("#users") do
-#         fill_in 'Email', with: 'user@example.com'
-#         fill_in 'Password', with: 'password'
-#       end
-#       click_button 'Sign in'
-#       expect(page).to have_content 'Success'
-#     end
-#   end
-require 'rails_helper'
-
  RSpec.describe 'Login page', type: :system do
    before do
      User.create(id: 1, name: 'name', email: 'name@name.com',
@@ -63,7 +46,5 @@ require 'rails_helper'
 
       end
 
-
-
          end
-         end
+    end
