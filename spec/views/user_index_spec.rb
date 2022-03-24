6eb0d0ce5856d0c19c2eb44860b23ff338006315
Anibal Amoroso <daniel_amoroso31@hotmail.com>
2022-03-24 15:27:37 -0300

    add tests to users/index

--- spec/views/user_index_spec.rb
+++ spec/views/user_index_spec.rb
@@ -1,2 +1,42 @@
-require 'rails_helper'
+ require 'rails_helper'
+
+ RSpec.describe 'User index page', type: :system do
+   before do
+   first_user = User.create(id: 1, name: 'name', email: 'name@name.com',
+        photo: 'https://images.pexels.com/photos/6171563/pexels-photo-6171563.jpeg  width = "300" height = "300"',
+        password: 'password', posts_counter: 2)
+
+
+      User.create(id: 2, name: 'amel', email: 'amel@amel.com',
+                  photo: 'https://randomuser.me/api/portraits/men/55.jpg',
+                password: '123456', posts_counter: 6)
+  end
+
+  describe 'Visiting the users index page' do
+    before :each do
+      visit '/users'
+    end
+
+     it 'should display the username of all users' do
+       expect(page).to have_text('name')
+       expect(page).to have_text('amel')
+     end
+
+     it 'should display the profile picture of all users' do
+       expect(page).to have_css('img')
+     end
+
+     it 'should display the number of posts of all users' do
+       expect(page).to have_text('Number of posts: 2')
+      expect(page).to have_text('Number of posts: 6')
+     end
+
+     it 'should redirect to users page when clicking a user' do
+      click_link('name')    
+      expect(page).to have_current_path('/users/1')
+
+   
+   end
+   end
+ end
 
